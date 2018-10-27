43891ca2593253eebce3e02807dd05326b35bacc
kimYSang <qwdbstkd@naver.com>
2018-10-27 18:27:39 +0900

    mvc2

---
+++ src/main/java/kr/ac/skuniv/SampleController.java
@@ -0,0 +1,16 @@
+package kr.ac.skuniv;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class SampleController {
+
+
+    @GetMapping("/hello")
+    public String hello(Model model){
+        model.addAttribute("name","yunsang");
+        return "hello";
+    }
+}
---
+++ src/test/java/kr/ac/skuniv/SampleControllerTest.java
@@ -0,0 +1,53 @@
+package kr.ac.skuniv;
+
+import com.gargoylesoftware.htmlunit.Page;
+import com.gargoylesoftware.htmlunit.WebClient;
+import com.gargoylesoftware.htmlunit.html.HtmlHeading1;
+import com.gargoylesoftware.htmlunit.html.HtmlPage;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.hamcrest.Matchers.containsString;
+import static org.hamcrest.Matchers.is;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@RunWith(SpringRunner.class)
+@WebMvcTest(SampleController.class)   // 테스트 하려는 클래스
+public class SampleControllerTest {
+
+   @Autowired
+    MockMvc mockMvc;
+
+   /*
+    @Test
+    public void hello() throws Exception {
+        // 요청 "/hello"
+        // 응답
+        // - 모델 name : yunsang
+        // - 뷰 이름 : hello
+
+        mockMvc.perform(get("/hello"))
+                .andExpect(status().isOk())
+                .andDo(print())
+                .andExpect(view().name("hello"))
+                .andExpect(model().attribute("name", is("yunsang")))
+                .andExpect(content().string(containsString("yunsang")));
+    }*/
+
+    @Autowired
+    WebClient webClient;
+
+    @Test
+    public void hello() throws Exception{
+        HtmlPage page = webClient.getPage("/hello");
+        HtmlHeading1 h1 = page.getFirstByXPath("//h1");
+        assertThat(h1.getTextContent()).isEqualToIgnoringCase("yunsang");
+    }
+}
